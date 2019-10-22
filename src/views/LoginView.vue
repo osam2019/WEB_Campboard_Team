c6f993ae45058d22be4f00976a10b8df7c9e6792
rud1676 <rud167637@gmail.com>
2019-10-22 11:38:13 +0900

    make LoginView by layout

--- src/views/LoginView.vue
+++ src/views/LoginView.vue
@@ -1,9 +1,54 @@
 <template>
-    <div>
-        <user-login></user-login>
-            여기에 id, pw 로그인 버튼 추가
-        <v-btn color="primary">Primary</v-btn>
-    </div>
+  <v-app id="inspire">
+    <v-content>
+      <v-container
+        class="fill-height"
+        fluid
+      >
+        <v-row
+          align="center"
+          justify="center"
+        >
+          <v-col
+            cols="12"
+            sm="8"
+            md="4"
+          >
+            <v-card class="elevation-12">
+              <v-toolbar
+                color="black"
+                dark
+                style="text-align:center"
+              >
+                <v-toolbar-title>CampBoard</v-toolbar-title>
+              </v-toolbar>
+              <v-card-text>
+                <v-form>
+                  <v-text-field
+                    label="군번"
+                    name="login"
+                    type="text"
+                  ></v-text-field>
+
+                  <v-text-field
+                    id="password"
+                    label="패스워드"
+                    name="password"
+                    type="password"
+                  ></v-text-field>
+                </v-form>
+              </v-card-text>
+              <v-card-actions>
+                <v-spacer></v-spacer>
+                <v-btn color="primary">Login</v-btn>
+              </v-card-actions>
+            </v-card>
+          </v-col>
+        </v-row>
+      </v-container>
+    </v-content>
+    <user-login></user-login>
+  </v-app>
     <!-- <button class="LoginButton">Login</button> -->
 </template>
 
