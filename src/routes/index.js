d67df657539f2744914d0fbfd240b8d5284fccab
Younghoon Jeong <yhjeong0422@gmail.com>
2019-10-22 16:13:19 +0900

    commit for dev

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,47 +1,52 @@
-import Vue from 'vue';
-import VueRouter from 'vue-router';
-import LoginView from '../views/LoginView.vue';
-import MainView from '../views/MainView.vue';
+import Vue from "vue";
+import VueRouter from "vue-router";
+import LoginView from "../views/LoginView.vue";
+import MainView from "../views/MainView.vue";
+import DevView from "../views/DevView.vue";
 
 Vue.use(VueRouter);
 
 export const router = new VueRouter({
-    mode: 'history', //url #값 제거 하는 기능
+  mode: "history", //url #값 제거 하는 기능
 
-    routes: [
-        {
-            path: '/',//redirect 바로 /news url로 access
-            redirect: '/loginview',
-        },
-        {
-            // path: url 주소
-            path: '/loginview',
-            name: 'loginview',
-            // coomponent: url 주소로 갔을 때 표시될 컴포넌트
-            // component: NewsView,
-            component: LoginView,
-        },
-        {
-            path: '/mainview',
-            name: 'mainview',
-            // component: AskView,
-            component: MainView
-        },
-        // {
-        //     path: '/jobs',
-        //     name: 'jobs',
-        //     // component: JobsView,
-        //     component: createListView('JobSView'),
-        // },
-        // {
-        //     path: '/user/:id',
-        //     component: UserView,
-        // },
-        // {
-        //     path: '/item/:id',
-        //     component: ItemView,
-        // },
-
-
-    ]
+  routes: [
+    {
+      path: "/", //redirect 바로 /news url로 access
+      redirect: "/loginview"
+    },
+    {
+      // path: url 주소
+      path: "/loginview",
+      name: "loginview",
+      // coomponent: url 주소로 갔을 때 표시될 컴포넌트
+      // component: NewsView,
+      component: LoginView
+    },
+    {
+      path: "/mainview",
+      name: "mainview",
+      // component: AskView,
+      component: MainView
+    },
+    {
+      path: "/devview",
+      name: "devview",
+      // component: AskView,
+      component: DevView
+    }
+    // {
+    //     path: '/jobs',
+    //     name: 'jobs',
+    //     // component: JobsView,
+    //     component: createListView('JobSView'),
+    // },
+    // {
+    //     path: '/user/:id',
+    //     component: UserView,
+    // },
+    // {
+    //     path: '/item/:id',
+    //     component: ItemView,
+    // },
+  ]
 });
---
+++ src/views/DevView.vue
@@ -0,0 +1,15 @@
+<template>
+  <div>
+    <basic-layout></basic-layout>
+  </div>
+</template>
+
+<script>
+import BasicLayout from "../components/layouts/BasicLayout";
+export default {
+  components: { BasicLayout }
+};
+</script>
+
+<style>
+</style>
