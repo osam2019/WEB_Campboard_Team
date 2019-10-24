38d850d4dc5f03b575afa35d77751fb880b003cd
kyungphill <phill0114@naver.com>
2019-10-24 22:13:04 +0900

    add login img

--- src/components/Login/UserLogin.vue
+++ src/components/Login/UserLogin.vue
@@ -2,25 +2,30 @@
   <v-app class="header">
     <v-container class="fill-height">
       <v-row>
-        <v-col class="d-flex justify-center">
-          <v-card style="width:450px">
-            <v-toolbar color="primary" dark style="text-align:center">
-              <v-toolbar-title>CampBoard</v-toolbar-title>
-            </v-toolbar>
-            <v-card-text>
-              <v-form>
-                <v-text-field label="군번" v-model="usernum" type="text"></v-text-field>
-                <v-text-field id="password" label="패스워드" v-model="password" type="password"></v-text-field>
-              </v-form>
-            </v-card-text>
-            <v-card-actions>
-              <router-link to="/register">
-                <v-btn color="primary">Register</v-btn>
-              </router-link>
-              <v-spacer></v-spacer>
-              <v-btn color="primary" @click="Login({usernum,password})">Login</v-btn>
-            </v-card-actions>
-          </v-card>
+        <v-col class="d-flex flex-column justify-center">
+          <v-layout class="d-flex justify-center mb-10">
+            <img style="left:50%;width:200px;height:240px" src="../../asset/campboard.png" />
+          </v-layout>
+          <div class="d-flex justify-center">
+            <v-card style="width:450px">
+              <v-toolbar color="primary" dark style="text-align:center">
+                <v-toolbar-title>CampBoard</v-toolbar-title>
+              </v-toolbar>
+              <v-card-text>
+                <v-form>
+                  <v-text-field label="군번" v-model="usernum" type="text"></v-text-field>
+                  <v-text-field id="password" label="패스워드" v-model="password" type="password"></v-text-field>
+                </v-form>
+              </v-card-text>
+              <v-card-actions>
+                <router-link to="/register">
+                  <v-btn color="primary">Register</v-btn>
+                </router-link>
+                <v-spacer></v-spacer>
+                <v-btn color="primary" @click="Login({usernum,password})">Login</v-btn>
+              </v-card-actions>
+            </v-card>
+          </div>
         </v-col>
       </v-row>
     </v-container>
