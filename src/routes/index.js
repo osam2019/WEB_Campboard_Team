80d5aa58d2ff97f07ad88c840cdccc14633a3041
Younghoon Jeong <yhjeong0422@gmail.com>
2019-10-24 20:36:41 +0900

    dasdfasdf

--- src/routes/index.js
+++ src/routes/index.js
@@ -10,7 +10,7 @@ import QuestionView from "../views/QuestionView.vue";
 import HeartLetterView from "../views/HeartLetterView.vue";
 import Vacation from "../views/VacationView.vue";
 //import for Login
-import Regist from "../views/RegistView.vue";
+import Register from "../views/RegistView.vue";
 Vue.use(VueRouter);
 
 export const router = new VueRouter({
@@ -30,9 +30,9 @@ export const router = new VueRouter({
     },
 
     {
-      path: "/Regist",
-      name: "Regist",
-      component: Regist
+      path: "/register",
+      name: "register",
+      component: Register
     },
     {
       // path: url 주소
